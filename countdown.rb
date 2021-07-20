9790beb82cadb7474a2aab70d8f1c9d3862b6601
sopharsogood <justanothersophie@gmail.com>
2021-07-20 17:39:01 -0400

    commit tree 715c58c70c50334766c2f76b4ce92f552cc4536e

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds_remaining)
+    while seconds_remaining > 0
+        puts "#{seconds_remaining} SECOND(S)!"
+        seconds_remaining -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_remaining)
+    while seconds_remaining > 0
+        puts "#{seconds_remaining} SECOND(S)!"
+        sleep 1
+        seconds_remaining -= 1
+    end
+    "HAPPY NEW YEAR!"
 end
